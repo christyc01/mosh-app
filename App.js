51d6bb68cc8cb08c7502cb9c7af14a55a8129475
Christy <57055910+christyc01@users.noreply.github.com>
2021-10-03 16:29:52 -0600

    Initial commit - welcome and image screens connected to App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen'
+import ViewImageScreen from './screens/ViewImageScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <WelcomeScreen />
+      <ViewImageScreen />
     </View>
   );
 }
---
+++ screens/ViewImageScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, ImageBackground } from "react-native";
+
+function ViewImageScreen() {
+  return (
+    <View>
+      <Text>This is the View Image Screen</Text>
+    </View>
+  )
+}
+
+export default ViewImageScreen;
